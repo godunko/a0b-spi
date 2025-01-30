5b9b3a82fe8d63a41e17965c91372c6a02fee89c
Vadim Godunko <vgodunko@gmail.com>
2025-01-30 20:33:05 +0400

    Block operations.

--- source/a0b-spi.ads
+++ source/a0b-spi.ads
@@ -1,10 +1,12 @@
 --
---  Copyright (C) 2024, Vadim Godunko
+--  Copyright (C) 2024-2025, Vadim Godunko
 --
 --  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 --
 
 pragma Restrictions (No_Elaboration_Code);
+
+with System;
 
 with A0B.Callbacks;
 with A0B.Types;
@@ -12,6 +14,22 @@ with A0B.Types;
 package A0B.SPI
   with Preelaborate
 is
+
+   type Buffer_Descriptor is record
+      Address     : System.Address;
+      Size        : A0B.Types.Unsigned_32;
+      Transferred : A0B.Types.Unsigned_32;
+      State       : A0B.Operation_Status;
+   end record;
+   --  Descriptor of the transmit/receive buffer.
+   --
+   --  @component Address      Address of the first byte of the buffer memory
+   --  @component Size         Size of the buffer in bytes
+   --  @component Transferred  Number of byte transferred by the operation
+   --  @component State        State of the operation
+
+   type Buffer_Descriptor_Array is
+     array (A0B.Types.Unsigned_32 range <>) of aliased Buffer_Descriptor;
 
    --   type SPI_Slave_Device is tagged;
 
@@ -48,6 +66,23 @@ is
 
    not overriding procedure Release_Device
      (Self : in out SPI_Slave_Device) is abstract;
+
+   not overriding procedure Transmit
+     (Self             : in out SPI_Slave_Device;
+      Transmit_Buffers : in out A0B.SPI.Buffer_Descriptor_Array;
+      On_Finished      : A0B.Callbacks.Callback;
+      Success          : in out Boolean) is abstract;
+   --  Transmit data to/from the device. Received data is ignored. Given
+   --  callback will be called when transmission is done.
+
+   not overriding procedure Receive
+     (Self            : in out SPI_Slave_Device;
+      --  Transmit_Placeholder : A0B.Types.Unsigned_8;
+      Receive_Buffers : in out A0B.SPI.Buffer_Descriptor_Array;
+      On_Finished     : A0B.Callbacks.Callback;
+      Success         : in out Boolean) is abstract;
+   --  Receive data to/from the device. Transmitted data is zeros. Given
+   --  callback will be called when transmission is done.
 
    --  type SPI_Software_Selection_Controlled_Slave_Device is limited interface
    --    and SPI_Slave_Device;
